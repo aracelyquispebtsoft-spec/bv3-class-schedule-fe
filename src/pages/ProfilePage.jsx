00f1d288dd3ad0b1975a49bfb1f2353cde14b6c0
Niv3k <condorinicolaskevin@gmail.com>
2026-09-16 18:26:07 -0400

    CLS-5:add navigation layour and module routes

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+function ProfilePage() {
+  return (
+    <h1 className="text-3xl font-bold text-slate-900">
+      Mi perfil
+    </h1>
+  )
+}
+
+export default ProfilePage
---
+++ src/pages/TeacherPage.jsx
@@ -0,0 +1,9 @@
+function TeacherPage() {
+  return (
+    <h1 className="text-3xl font-bold text-slate-900">
+      Docentes
+    </h1>
+  )
+}
+
+export default TeacherPage
