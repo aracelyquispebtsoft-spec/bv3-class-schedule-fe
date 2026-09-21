55d9e5e3b78679fa9f76904387d4a01a3aeb191d
Therengelan096 <anderson.alvarez.rengel.096@gmail.com>
2026-09-21 15:08:42 -0400

    CLS-15: CRUD de materias en pantalla (#6)

---
+++ src/services/subject.service.js
@@ -0,0 +1,46 @@
+import { HEADERS, URL_SUBJECTS, handleResponse } from './service'
+
+export const getAll = async () => {
+  const response = await fetch(URL_SUBJECTS, {
+    headers: HEADERS,
+  })
+
+  return handleResponse(response)
+}
+
+export const getOne = async (id) => {
+  const response = await fetch(`${URL_SUBJECTS}/${id}`, {
+    headers: HEADERS,
+  })
+
+  return handleResponse(response)
+}
+
+export const create = async (subject) => {
+  const response = await fetch(URL_SUBJECTS, {
+    method: 'POST',
+    headers: HEADERS,
+    body: JSON.stringify(subject),
+  })
+
+  return handleResponse(response)
+}
+
+export const update = async (id, subject) => {
+  const response = await fetch(`${URL_SUBJECTS}/${id}`, {
+    method: 'PUT',
+    headers: HEADERS,
+    body: JSON.stringify(subject),
+  })
+
+  return handleResponse(response)
+}
+
+export const remove = async (id) => {
+  const response = await fetch(`${URL_SUBJECTS}/${id}`, {
+    method: 'DELETE',
+    headers: HEADERS,
+  })
+
+  return handleResponse(response)
+}
